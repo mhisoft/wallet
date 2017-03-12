1ca6301e70c54aab175444fb6491b48b06d6ea72
mhisoft <mhisoft@github.com>
2017-03-11 21:07:50 -0500

    v1.1

--- src/main/java/org/mhisoft/wallet/WalletMain.java
+++ src/main/java/org/mhisoft/wallet/WalletMain.java
@@ -28,10 +28,10 @@ public class WalletMain {
 	 //three places to update the version number,
 	//here  in the code
 	// pom.xml and build.xml
-	public static final String version = "1.0.9";
+	public static final String version = "1.1.0";
 
 
-	public static final String build = "645";
+	public static final String build = "646";
 	public static final String BUILD_DETAIL = "MHISoft eVault " + version +" build " +  build;
 
 
