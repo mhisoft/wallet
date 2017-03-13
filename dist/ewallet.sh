cc1e408b220ce51cb1cd8ee55c77f23be6a20ee2
mhisoft <mhisoft@github.com>
2017-03-12 21:10:45 -0400

    update build file, add release target

--- dist/ewallet.sh
+++ dist/ewallet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./MAC/jdk1.8.0_51.jdk/Contents/Home/bin/java -jar ./ewallet-1.0.jar "$1" "$2" "$3" "$4" "$5"
+./MAC/jdk1.8.0_51.jdk/Contents/Home/bin/java -jar ./ewallet-1.1.jar "$1" "$2" "$3" "$4" "$5"
