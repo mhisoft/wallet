21c0b44efeed26d9f24ec9ee6490b0f45e9802ca
MHISoft <tony.zxue@gmail.com>
2018-03-05 17:11:11 -0500

    build 5b90e4f

--- src/main/java/org/mhisoft/wallet/WalletMain.java
+++ src/main/java/org/mhisoft/wallet/WalletMain.java
@@ -41,7 +41,7 @@ public class WalletMain {
 	public static final String version = "1.3.6";
 
 
-	public static final String build = "[e7f83c3] 3/5/2018";
+	public static final String build = "[5b90e4f] 3/5/2018";
 	public static final String BUILD_DETAIL = "MHISoft eVault " + version +" build" +  build;
 
 
